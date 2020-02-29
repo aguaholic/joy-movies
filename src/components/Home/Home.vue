de925dff13bbc3b7bfd58579dda4c9885dd5c6f4
aguaholic <joyterra@gmail.com>
2020-02-29 16:45:02 +0100

    Add Home component

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <navbar></navbar>
+    <list></list>
+    <footer></footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home',
+});
+</script>
