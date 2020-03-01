f36e9f4489efce7f4682c7d21eae1d321c6627c6
aguaholic <joyterra@gmail.com>
2020-03-01 17:54:13 +0100

    Fetch data from movie API

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -7,26 +7,67 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
+import axios from 'axios'
 import NavBar from '../NavBar.vue'
 import List from './List.vue'
+
+interface Item {
+  title: string,
+  src: string, 
+  rating: number,
+  releaseYear: string
+}
 
 export default Vue.extend({
   name: 'Home',
     data() {
       return {
-        items: [
-          { title: 'A', src:"https://picsum.photos/300/300/?image=20", rating:"10", releaseYear:"1988"},
-          { title: 'B', src:"https://picsum.photos/300/300/?image=20", rating:"3", releaseYear:"1998"},
-          { title: 'C', src:"https://picsum.photos/300/300/?image=20", rating:"5", releaseYear:"2008"},
-          { title: 'D', src:"https://picsum.photos/300/300/?image=20", rating:"6", releaseYear:"2018"},
-          { title: 'E', src:"https://picsum.photos/300/300/?image=20", rating:"8", releaseYear:"1978"}
-        ]
+        items: null as Item[] | null
       }
+  },
+  methods: {
+    fetchItems() {
+      axios.get('https://api.themoviedb.org/3/search/movie/?api_key=a109923888458bdee8244628cbd0abb2&page=1&language=en-US&query=joy&include_adult=false')
+        .then(response => {
+          this.items = response.data.results
+          .slice(0, 10)
+          .map(item => {
+            if (item.backdrop_path) {
+              return { 
+                title: item.title,
+                src: 'https://image.tmdb.org/t/p/w500'+ item.backdrop_path, 
+                rating: item.vote_average,
+                releaseYear: item.release_date
+              }
+            } else if (item.poster_path){
+              return { 
+                title: item.title,
+                src: 'https://image.tmdb.org/t/p/w500'+ item.poster_path, 
+                rating: item.vote_average,
+                releaseYear: item.release_date
+              }
+            } else {
+              return { 
+                title: item.title,
+                src: null, 
+                rating: item.vote_average,
+                releaseYear: item.release_date
+              }
+            }
+          })
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+  },
+  mounted() {
+    this.fetchItems()
   },
   components: {
     NavBar,
     List,
-  }
+  },
 });
 </script>
