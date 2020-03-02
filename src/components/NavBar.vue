5cd45e23a7ff4d5d87ff504859460a009ea3534b
aguaholic <joyterra@gmail.com>
2020-03-02 13:24:07 +0100

    Add router link to navigation bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-      <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item  to="/">Home</b-nav-item>
       </b-navbar-nav> 
       </b-collapse>
       </b-navbar>
